48b585c3a2cccce0e37a2501b6ed334cb2958ef9
Tristan Cacqueray <tdecacqu@redhat.com>
2024-06-02 08:17:22 -0400

    Add PwMon demo

---
+++ pipewire/examples/PwMon.hs
@@ -0,0 +1,17 @@
+module Main (main) where
+
+import Pipewire qualified as PW
+
+main :: IO ()
+main = PW.withInstance () printEvent \pwInstance -> do
+    print =<< PW.pw_main_loop_run pwInstance.mainLoop
+  where
+    printEvent ev _ = do
+        case ev of
+            PW.Added pwid name props -> do
+                putStrLn $ "added: " <> show pwid
+                putStrLn $ " type: " <> show name
+                mapM_ (putStrLn . mappend " " . show) =<< PW.spaDictRead props
+            PW.Removed pwid -> do
+                putStrLn $ "removed: " <> show pwid
+        putStrLn ""
